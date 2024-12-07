6c3f50e6363658210b6af867f32b526064728663
SALAHEDDIN47 <salaheddinejalloul538@gmail.com>
2024-12-07 23:18:36 +0100

    adding some animations

--- script.js
+++ script.js
@@ -83,7 +83,25 @@ const keys = {
   }
 };
 
+const movePlayer = (key, xVelocity, isPressed) => {
+  if (!isCheckpointCollisionDetectionActive) {
+    player.velocity.x = 0;
+    player.velocity.y = 0;
+    return;
+  }
 
+
+  switch (key) {
+    case "ArrowLeft":
+      keys.leftKey.pressed = isPressed;
+      if (xVelocity === 0) {
+        player.velocity.x = xVelocity;
+      }
+        player.velocity.x -= xVelocity;
+      break;
+  }
+
+}
 
 
 const startGame = () => {
