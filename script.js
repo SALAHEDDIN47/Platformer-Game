f8d6e553efcb4b973500ca5ce3b3cb6c80451dd0
SALAHEDDIN47 <salaheddinejalloul538@gmail.com>
2024-12-08 16:14:08 +0100

    adding some animations

--- script.js
+++ script.js
@@ -97,7 +97,16 @@ const movePlayer = (key, xVelocity, isPressed) => {
       if (xVelocity === 0) {
         player.velocity.x = xVelocity;
       }
-        player.velocity.x -= xVelocity;
+      player.velocity.x -= xVelocity;
+      break;
+    case "ArrowUp":
+      player.velocity.y -= 8;
+      break;
+      case " ":
+      player.velocity.y -= 8;
+      break;
+      case "Spacebar":
+      player.velocity.y -= 8;
       break;
   }
 
