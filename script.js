62d3fc63301ac646c86b8d8da9b88c9440249c44
SALAHEDDIN47 <salaheddinejalloul538@gmail.com>
2025-01-23 17:02:47 +0100

    working on the script

--- script.js
+++ script.js
@@ -116,11 +116,16 @@ const platforms = platformPositions.map(
   (platform) => new Platform(platform.x, platform.y)
 );
 
-const checkpointPositions = [ 
-  {x: 1170, y: proportionalSize(80), z: 1},
-  {x: 2900, y: proportionalSize(330), z: 2 },
-  {x: 4800, y: proportionalSize(80), z: 3 }
-]
+
+const checkpointPositions = [
+  { x: 1170, y: proportionalSize(80), z: 1 },
+  { x: 2900, y: proportionalSize(330), z: 2 },
+  { x: 4800, y: proportionalSize(80), z: 3 },
+];
+
+const checkpoints = checkpointPositions.map(
+  (checkpoint) => new CheckPoint(checkpoint.x, checkpoint.y, checkpoint.z)
+);
 
 const animate = () => {
   requestAnimationFrame(animate);
@@ -129,6 +134,8 @@ const animate = () => {
   platforms.forEach((platform) => {
     platform.draw();
   });
+
+
 
   player.update();
 
